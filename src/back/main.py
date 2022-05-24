b0c31c82a3014454964388f8c931845056b56ebc
Quoc Viet NGo <viet.ngo@aalto.fi>
2022-05-25 00:42:33 +0300

    tmdb added
    added the tmdb code

--- src/back/main.py
+++ src/back/main.py
@@ -1,21 +1,18 @@
+
 from fastapi import FastAPI, Path, Query, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from typing import Optional
 import requests
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 api_key = '15d2ea6d0dc1d476efbca3eba2b9bbfb'
 recommendation_payload = {'api_key': api_key, 'language': 'en-US'}
-
-
+r = requests.get('https://api.themoviedb.org/3/genre/movie/list',
+                            params=recommendation_payload, timeout=10).json()['genres']
+movie_genres=dict()
+for genre in r:
+    movie_genres[str(genre['id'])]=genre['name']
 @app.get("/recommendation/{title}")
 async def recommendation(title: str = Path(None, description="Title of the basis movie (mandatory)"),
                          #limit: Optional[int] = Query(None, description="Max nr of movies to list."),
@@ -40,7 +37,8 @@ async def recommendation(title: str = Path(None, description="Title of the basis
         raise HTTPException(status_code=r.status_code, detail="Some error")
 
     if(r.json()['total_results'] == 0):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movie found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No movie found")
 
     movie_id = r.json()['results'][0]['id']
 
@@ -48,7 +46,7 @@ async def recommendation(title: str = Path(None, description="Title of the basis
 
     if(not r.ok):
         raise HTTPException(status_code=r.status_code, detail="Some error")
-
+    
     results = r.json()['results']
     for movie in results:
         movie['poster_path'] = 'https://image.tmdb.org/t/p/original' + movie['poster_path']
@@ -71,4 +69,9 @@ async def recommendation(title: str = Path(None, description="Title of the basis
         for provider in movie['providers']:
             provider['logo_path'] = 'https://image.tmdb.org/t/p/original' + provider['logo_path']
 
+        genres=[]
+        for genre in movie['genre_ids']:
+           genres.append(movie_genres[str(genre)])
+        movie['genre_ids']=genres
+        movie['release_date']=movie['release_date'][0:4]
     return results
